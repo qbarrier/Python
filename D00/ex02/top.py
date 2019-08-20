30d70cb7d4cd863734b4d0bc9284e1deea4dd51e
Quentin Barrier <qbarrier@z3r6p3.le-101.fr>
2019-08-20 15:48:53 +0200

    ex02 3

---
+++ D00/ex02/top.py
@@ -0,0 +1,3 @@
+music = ["Tout petit Moineau", "C'est", "Violent & Funky", "Expect Us", "Fils de P."]
+
+print (music)
---
+++ D00/ex03/loop.py
@@ -0,0 +1,5 @@
+music = ["Tout petit Moineau", "C'est", "Violent & Funky", "Expect Us", "Fils de P."]
+
+
+for index, val in enumerate(music) :
+    print ("My favorite title", index + 1, "is", val + ".")
