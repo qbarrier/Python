a16eac2021cae15a0082ba3b4dff24a28246c9c8
Quentin Barrier <qbarrier@z3r6p3.le-101.fr>
2019-08-20 15:52:33 +0200

    ex04

---
+++ D00/ex04/pioneers_dict.py
@@ -0,0 +1,27 @@
+dates = [
+            ('Alan Turing' , '1912'),
+            ('Ada Lovelace' , '1815'),
+            ('Tim Berners-Lee' , '1955'),
+            ('Grace Hopper' , '1906'),
+            ('Denis Ritchie' , '1941'),
+            ('Richard Stallman' , '1953'),
+            ('Guido van Rossum' , '1956'),
+            ('Radia Perlman' , '1951'),
+            ('Larry Page' , '1973'),
+            ('Mary Kenneth Keller' , '1913'),
+            ('Dorothy Vaughan' , '1910'),
+            ('Annie Easley' , '1933'),
+            ('Ian Murdock' , '1973'),
+            ('Carol Shaw' , '1955'),
+            ('Aaron Swartz' , '1986'),
+            ('Hedy Lamarr' , '1914'),
+            ('Katherine Johnson' , '1918'),
+            ('Jimmy Wales' , '1966'),
+            ('Jean E. Sammet' , '1928'),
+            ('Charles Babbage' , '1791'),
+            ('Margaret Hamilton', '1936'),
+            ('Vint Cerf' , '1943'),
+]
+
+for x in dates :
+    print(x[1], ":", x[0])
